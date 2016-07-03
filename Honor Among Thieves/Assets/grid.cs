5f81ea58af50bda906856f1d0dfd079bbae32cc0
Szymon Rzosiński <Szymon Rzosiski>
2016-07-03 22:51:50 +0200

    sdfsdfsdf

---
+++ Honor Among Thieves/Assets/grid.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class grid : MonoBehaviour {
+	public int x;
+	public int y;
+	public GameObject go;
+	public GameObject gox;
+	public GameObject goxx;
+	public GameObject tmpg;
+	//0.96
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < x; i++) {
+			for (int n = 0; n < y; n++) {
+				Instantiate (go, new Vector3 (this.transform.position.x + (0.96F * i),this.transform.position.y + (0.96F * n),1), this.transform.rotation);
+				if (Random.Range (0, 100) < 2 || Random.Range (0, 100) > 98) {
+					tmpg = Instantiate (gox, new Vector3 (this.transform.position.x + (0.96F * i),this.transform.position.y + (0.96F * n),1), this.transform.rotation) as GameObject;
+					tmpg.GetComponent<SpriteRenderer> ().sortingOrder = 1;
+				}else if (Random.Range (0, 100) < 88  && Random.Range (0, 100) > 84) {
+					tmpg = Instantiate (goxx, new Vector3 (this.transform.position.x + (0.96F * i),this.transform.position.y + (0.96F * n),1), this.transform.rotation) as GameObject;
+					tmpg.GetComponent<SpriteRenderer> ().sortingOrder = 1;
+				}
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Honor Among Thieves/Assets/scripts/player/mvc.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mvc : MonoBehaviour {
+	public Animator par;
+	public Rigidbody2D r2d;
+	public int sm;
+	void Awake(){
+		par = this.GetComponent<Animator>();
+		r2d = this.GetComponent<Rigidbody2D> ();
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	void FixedUpdate(){
+
+	}
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("SM")) {
+			if (sm == 1) {
+				sm = sm*2;
+			} else {
+				sm = sm/2;
+			}
+		}
+		if (Input.GetAxisRaw ("Vertical") > 0.1F) {
+			r2d.velocity = new Vector2 (r2d.velocity.x, Input.GetAxisRaw ("Vertical")*sm);
+			par.speed = Input.GetAxisRaw ("Vertical") * sm;
+			par.SetInteger ("wv", 1);
+		} else if (Input.GetAxisRaw ("Vertical") < -0.1F) {
+			r2d.velocity = new Vector2 (r2d.velocity.x, Input.GetAxisRaw ("Vertical")*sm);
+			par.speed = Input.GetAxisRaw ("Vertical") * sm *-1;
+			par.SetInteger ("wv", -1);
+		} else {
+			r2d.velocity = new Vector2 (r2d.velocity.x, 0);
+			par.SetInteger ("wv", 0);
+		}
+		if(Input.GetAxisRaw("Horizontal") > 0.1F){
+			r2d.velocity = new Vector2 (Input.GetAxisRaw("Horizontal")*sm, r2d.velocity.y);
+			par.speed = Input.GetAxisRaw ("Horizontal") * sm;
+			par.SetInteger ("wh", 1);
+		}else if(Input.GetAxisRaw("Horizontal") < -0.1F){
+			r2d.velocity = new Vector2 (Input.GetAxisRaw("Horizontal")*sm, r2d.velocity.y);
+			par.speed = Input.GetAxisRaw ("Horizontal") * sm *-1;
+			par.SetInteger ("wh", -1);
+		} else {
+			r2d.velocity = new Vector2 (Input.GetAxisRaw("Horizontal")*sm, r2d.velocity.y);
+			par.SetInteger ("wh", 0);
+		}
+	}
+}
